4cf4200c078a716b012daa5f233fa46ceb7c71fb
Mikhail Smirnov <m2004029@edu.misis.ru>
2025-02-18 10:57:43 +0300

    исправлены отступы в решениях

--- src/pages/HomePage/Solutions.jsx
+++ src/pages/HomePage/Solutions.jsx
@@ -11,7 +11,7 @@ const Solutions = () => {
         <Link className={styles.landscape} to="products/Ландшафт">
           <section className={styles.landscape}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Кинетикомоторные площадки</h3>
                 <p>
                   Развитие координации <br />и движения
@@ -27,7 +27,7 @@ const Solutions = () => {
         <Link className={styles.tramptek} to="products/Трамптек">
           <section className={styles.tramptek}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Трамптек</h3>
                 <p>Уличные батуты</p>
               </div>
@@ -41,7 +41,7 @@ const Solutions = () => {
         <Link className={styles.mini} to="products/Мини">
           <section className={styles.mini}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Мини</h3>
                 <p>Обучающие площадки для детей до 7 лет </p>
               </div>
@@ -55,7 +55,7 @@ const Solutions = () => {
         <Link className={styles.towers} to="products/Башни">
           <section className={styles.towers}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Башни</h3>
                 <p>Универсальные высотные доминанты </p>
               </div>
@@ -69,7 +69,7 @@ const Solutions = () => {
         <Link className={styles.parkfit} to="products/Паркфит">
           <section className={styles.parkfit}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Паркфит</h3>
                 <p>Уличный спорт</p>
               </div>
@@ -83,7 +83,7 @@ const Solutions = () => {
         <Link className={styles.individualProjects} to="products">
           <section className={styles.individualProjects}>
             <div className={styles.textBlock}>
-              <div>
+              <div className={styles.textBlockTitle}>
                 <h3>Индивидуальные проекты</h3>
                 <p>Реализуем уникальные технологические решения </p>
               </div>
