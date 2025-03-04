ef6948bd06b0a0426366c9bd9a43f7644e91d265
Mikhail Smirnov <m2004029@edu.misis.ru>
2025-03-04 17:23:22 +0300

    сделал дизайн выпадающего списка в группах

--- src/pages/Card/GroupSection.jsx
+++ src/pages/Card/GroupSection.jsx
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles/Card.module.css";
 import { slugify } from "transliteration";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 const GroupSection = ({ groupProducts, groupName, navigate }) => {
   if (!groupProducts || groupProducts.length === 0) {
     return null;
   }
 
+  const [isOpen, setIsOpen] = useState(false);
+  const currentItem = groupProducts.find((p) => p.isCurrent);
+
+  const handleItemClick = (cardId, productName) => {
+    if (cardId) {
+      const titleSlug = slugify(productName || "bez-nazvaniya", {
+        lowercase: true,
+        separator: "-",
+      });
+      navigate(`/card/${cardId}/${titleSlug}`);
+    }
+    setIsOpen(false);
+  };
+
   return (
     <div className={styles.groupContainer}>
       <h4>Габариты</h4>
-      <ul className={styles.groupList}>
-        {groupProducts.map(({ id, card, cardId, isCurrent, productName }) => (
-          <li
-            key={id}
-            onClick={() => {
-              if (cardId) {
-                const titleSlug = slugify(productName || "bez-nazvaniya", {
-                  lowercase: true,
-                  separator: "-",
-                });
-                navigate(`/card/${cardId}/${titleSlug}`);
-              }
-            }}
-            className={isCurrent ? styles.currentGroupItem : styles.groupItem}
-          >
-            {card?.size || "Нет габаритов"}
-          </li>
-        ))}
-      </ul>
+
+      <div className={styles.dropdownContainer}>
+        <div
+          className={styles.dropdownHeader}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <div className={styles.currentDimension}>
+            {currentItem?.card?.size || "Нет габаритов"}
+          </div>
+
+          <div className={styles.arrowContainer}>
+            {isOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+          </div>
+        </div>
+
+        {isOpen && (
+          <ul className={styles.dropdownList}>
+            {groupProducts
+              .filter((item) => !item.isCurrent)
+              .map(({ id, card, cardId, productName }) => (
+                <li
+                  key={id}
+                  onClick={() => handleItemClick(cardId, productName)}
+                  className={styles.groupItem}
+                >
+                  {card?.size || "Нет габаритов"}
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
