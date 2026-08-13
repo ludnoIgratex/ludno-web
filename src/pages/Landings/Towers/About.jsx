38c866e6b2545147b8c1db07164c2b1a11dc4b9e
Mikhail Smirnov <m2004029@edu.misis.ru>
2026-08-13 17:12:03 +0300

    added link to tower catalog

--- src/pages/Landings/Towers/About.jsx
+++ src/pages/Landings/Towers/About.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./styles/About.module.css";
 import { RiArrowRightDownLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 const About = () => {
+  const navigate = useNavigate();
+  const isTouchDevice = useMediaQuery({ query: "(hover: none)" });
+
+  const handleCatalogClick = () => {
+    if (isTouchDevice) {
+      navigate("/products?solutions=152");
+    } else {
+      navigate("/products/Башни");
+    }
+  };
+
   const handleDownloadClick = () => {
     window.open("https://disk.yandex.ru/d/bUyEqGKFb_jAOA", "_blank");
   };
@@ -18,6 +31,10 @@ const About = () => {
       </p>
 
       <div className={styles.linkWrapper}>
+        <div className={styles.linkContainer} onClick={handleCatalogClick}>
+          <RiArrowRightDownLine className={styles.arrow} />
+          <a>В каталог</a>
+        </div>
         <div className={styles.linkContainer} onClick={handleDownloadClick}>
           <RiArrowRightDownLine className={styles.arrow} />
           <a>Скачать pdf</a>
